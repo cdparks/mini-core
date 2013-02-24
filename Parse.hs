123855b1bb255b28fa7e0344548c9335e1582b63
Christopher Parks <christopher.daniel.parks@gmail.com>
2013-02-23 21:43:18 -0800

    Moving some stuff around; adding pNum which parses and converts a numeric string to Int

--- Parse.hs
+++ Parse.hs
@@ -13,6 +13,43 @@ type Parser a = [Token] -> [(a, [Token])]
 -- List of keywords
 keywords = ["let", "letrec", "case", "in", "of", "Pack"]
 
+{- Simple lexical analyis -}
+
+-- Scan string with look-ahead 1
+scan :: Int -> String -> [Token]
+scan lineno [] = [(lineno, "")]
+scan lineno s@(c:cs)
+    | c == '\n' = scan (lineno + 1) cs
+    | isSpace c = scan lineno cs
+    | isDigit c = let (digits, rest) = span isDigit s
+                  in (lineno, digits):scan lineno rest
+    | isAlpha c = let (ending, rest) = span isIdentifierChar cs
+                      identifier = c:ending
+                  in (lineno, identifier):scan lineno rest
+    | otherwise = scan' lineno s
+
+-- Scanning with look-ahead 1 didn't find anything so,
+-- try with look-ahead 2. Switch back to look-ahead 1 after.
+scan' :: Int -> String -> [Token]
+scan' lineno s@(c:c':cs)
+    | cc' == "--" = let rest = dropWhile (/='\n') cs
+                    in scan lineno rest
+    | cc' `elem` twoCharOps = (lineno, cc'):scan lineno cs
+    | otherwise = (lineno, [c]):scan lineno c'cs where
+      cc' = [c, c']
+      c'cs = c':cs
+scan' lineno s@(c:cs) = (lineno, [c]):scan lineno cs
+
+-- The first character of an identifier must be a letter. The
+-- rest can be letters, numbers or _
+isIdentifierChar :: Char -> Bool
+isIdentifierChar c = isAlphaNum c || c == '_'
+
+-- Operators consisting of 2 characters
+twoCharOps = filter ((==2) . length) $  map fst binaryOps
+
+{- Parser combinators -}
+
 -- Parse succeeds if predicate is satisfied
 pSat :: (String -> Bool) -> Parser String
 pSat pred ((_, token):tokens)
@@ -24,13 +61,18 @@ pSat pred []     = []
 pLit :: String -> Parser String
 pLit s = pSat (==s)
 
--- Try each parser on same input and combine matches
-pAlt :: Parser a -> Parser a -> Parser a
-pAlt p1 p2 tokens = (p1 tokens) ++ (p2 tokens)
-
 -- Succeeds if input is a variable
 pVar :: Parser String
 pVar = pSat (\token -> isAlpha(head(token)) && token `notElem` keywords)
+
+-- Succeeds if input is numeric and converts to integer
+pNum :: Parser Int
+pNum = pApply (pSat (all isDigit)) toInt where
+    toInt = read::(String -> Int)
+
+-- Try each parser on same input and combine matches
+pAlt :: Parser a -> Parser a -> Parser a
+pAlt p1 p2 tokens = (p1 tokens) ++ (p2 tokens)
 
 -- Parse input with first parser, parse remaining input with second
 -- parser, and combine matches
@@ -65,39 +107,6 @@ pBetween lhs p rhs = pThen (flip const) lhs (pThen const p rhs)
 -- Parse repeated input separated by delimiters (discarding delimiters)
 pOneOrMoreWithSep :: Parser a -> Parser b -> Parser [a]
 pOneOrMoreWithSep p1 p2 = pThen (:) p1 (pZeroOrMore (pThen (flip const) p2 p1))
-
--- Scan string with look-ahead 1
-scan :: Int -> String -> [Token]
-scan lineno [] = [(lineno, "")]
-scan lineno s@(c:cs)
-    | c == '\n' = scan (lineno + 1) cs
-    | isSpace c = scan lineno cs
-    | isDigit c = let (digits, rest) = span isDigit s
-                  in (lineno, digits):scan lineno rest
-    | isAlpha c = let (ending, rest) = span isIdentifierChar cs
-                      identifier = c:ending
-                  in (lineno, identifier):scan lineno rest
-    | otherwise = scan' lineno s
-
--- Scanning with look-ahead 1 didn't find anything so,
--- try with look-ahead 2. Switch back to look-ahead 1 after.
-scan' :: Int -> String -> [Token]
-scan' lineno s@(c:c':cs)
-    | cc' == "--" = let rest = dropWhile (/='\n') cs
-                    in scan lineno rest
-    | cc' `elem` twoCharOps = (lineno, cc'):scan lineno cs
-    | otherwise = (lineno, [c]):scan lineno c'cs where
-      cc' = [c, c']
-      c'cs = c':cs
-scan' lineno s@(c:cs) = (lineno, [c]):scan lineno cs
-
--- The first character of an identifier must be a letter. The
--- rest can be letters, numbers or _
-isIdentifierChar :: Char -> Bool
-isIdentifierChar c = isAlphaNum c || c == '_'
-
--- Operators consisting of 2 characters
-twoCharOps = filter ((==2) . length) $  map fst binaryOps
 
 {-
 -- Parse list of tokens into abstract syntax tree
