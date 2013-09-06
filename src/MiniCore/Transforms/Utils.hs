1abf9fd5ee42ec590d0502543152a1c55ecb1f4b
Christopher D. Parks <christopher.daniel.parks@gmail.com>
2013-09-05 21:29:27 -0700

    Clean up Transforms/Utils.hs and expose freeVarsExpr which allows one to annotate a single expression with free-variable information

--- src/MiniCore/Transforms/Utils.hs
+++ src/MiniCore/Transforms/Utils.hs
@@ -2,6 +2,7 @@ module MiniCore.Transforms.Utils where
 
 import MiniCore.Types
 import qualified Data.Set as Set
+import Data.List (foldl')
 
 -- Type synonyms for nodes annotated with free variables
 type FVProgram = AProgram Name (Set.Set Name)
@@ -9,60 +10,80 @@ type FVExpr = Annotated Name (Set.Set Name)
 type FVAlt = AAlt Name (Set.Set Name)
 
 -- Walk program and return a new AST annotated with each node's free variables
-freeVars :: Program -> FVProgram
-freeVars = map $ \(Combinator name args body) -> (name, args, walk (Set.fromList args) body)
+freeVars :: Program -> Stage FVProgram
+freeVars = mapM freeVars'
   where
-    walk :: Set.Set Name -> Expr -> FVExpr
-    walk vars (Num n) = (Set.empty, ANum n)
-    walk vars (Cons tag arity) = (Set.empty, ACons tag arity)
-    walk vars (Var v)
-        | v `Set.member` vars = (Set.singleton v, AVar v)
-        | otherwise           = (Set.empty, AVar v)
-    walk vars (BinOp op e1 e2) = walk vars (App (App (Var op) e1) e2)
-    walk vars (App e1 e2) = (vars', AApp e1' e2')
-      where
-        e1' = walk vars e1
-        e2' = walk vars e2
-        vars' = freeVarsOf e1' `Set.union` freeVarsOf e2'
-    walk vars (Lambda args body) = (vars', ALambda args body')
-      where
-        argSet = Set.fromList args
-        body' = walk (vars `Set.union` argSet) body
-        vars' = freeVarsOf body' `Set.difference` argSet
-    walk vars (Case expr alts) = (vars', ACase expr' alts')
-      where
-        expr' = walk vars expr
-        alts' = map (walkAlt vars) alts
-        vars' = freeVarsOf expr' `Set.union` (flattenSet $ map freeVarsOfAlt alts')
-    walk vars (Let recursive defs body) = (vars', ALet recursive defs' body')
-      where
-        binders = bindersOf defs
-        binderSet = Set.fromList binders
-        bodyVars = vars `Set.union` binderSet
-        rhsVars | recursive = bodyVars
-                | otherwise = vars
-        rhs' = map (walk rhsVars) $ bindeesOf defs
-        defs' = zip binders rhs'
-        freeInValues = flattenSet $ map freeVarsOf rhs'
-        defsFree | recursive = freeInValues `Set.difference` binderSet
-                 | otherwise = freeInValues
-        body' = walk bodyVars body
-        bodyFree = freeVarsOf body' `Set.difference` binderSet
-        vars' = defsFree `Set.union` bodyFree
+    freeVars' (Combinator name args body) =
+        do body' <- freeVarsExpr (Set.fromList args) body
+           return (name, args, body')
 
-    flattenSet :: Ord a => [Set.Set a] -> Set.Set a
-    flattenSet = foldl Set.union Set.empty
+-- Annotate expression with free variables
+freeVarsExpr :: Set.Set Name -> Expr -> Stage FVExpr
+freeVarsExpr vars (Num n) =
+    return (Set.empty, ANum n)
 
-    freeVarsOfAlt :: FVAlt -> Set.Set Name
-    freeVarsOfAlt (tag, args, rhs) = freeVarsOf rhs `Set.difference` Set.fromList args
+freeVarsExpr vars (Cons tag arity) =
+    return (Set.empty, ACons tag arity)
 
-    walkAlt :: Set.Set Name -> Alt -> FVAlt
-    walkAlt vars (tag, args, rhs) = (tag, args, rhs')
-      where
-        argSet = Set.fromList args
-        rhs' = walk (vars `Set.union` argSet) rhs
+freeVarsExpr vars (Var v)
+    | v `Set.member` vars = return (Set.singleton v, AVar v)
+    | otherwise           = return (Set.empty, AVar v)
+
+freeVarsExpr vars (BinOp op e1 e2) =
+    freeVarsExpr vars (App (App (Var op) e1) e2)
+
+freeVarsExpr vars (App e1 e2) =
+    do e1' <- freeVarsExpr vars e1
+       e2' <- freeVarsExpr vars e2
+       let vars' = freeVarsOf e1' `Set.union` freeVarsOf e2'
+       return (vars', AApp e1' e2')
+
+freeVarsExpr vars (Lambda args body) =
+    do let argSet = Set.fromList args
+       body' <- freeVarsExpr (vars `Set.union` argSet) body
+       let vars' = freeVarsOf body' `Set.difference` argSet
+       return (vars', ALambda args body')
+
+freeVarsExpr vars (Case expr alts) =
+    do expr' <- freeVarsExpr vars expr
+       alts' <- mapM (freeVarsAlt vars) alts
+       let vars' = freeVarsOf expr' `Set.union` (flattenSet $ map freeVarsOfAlt alts')
+       return (vars', ACase expr' alts')
+
+freeVarsExpr vars (Let recursive defs body) =
+    do let binders = bindersOf defs
+           binderSet = Set.fromList binders
+           bodyVars = vars `Set.union` binderSet
+           defVars
+              | recursive = bodyVars
+              | otherwise = vars
+       exprs <- mapM (freeVarsExpr defVars) $ bindeesOf defs
+       let defs' = zip binders exprs
+           freeInValues = flattenSet $ map freeVarsOf exprs
+           defsFree
+              | recursive = freeInValues `Set.difference` binderSet
+              | otherwise = freeInValues
+       body' <- freeVarsExpr bodyVars body
+       let bodyFree = freeVarsOf body' `Set.difference` binderSet
+           vars' = defsFree `Set.union` bodyFree
+       return (vars', ALet recursive defs' body')
+
+-- Annotate alternative with free vars
+freeVarsAlt :: Set.Set Name -> Alt -> Stage FVAlt
+freeVarsAlt vars (tag, args, expr) = 
+    do let argSet = Set.fromList args
+       expr' <- freeVarsExpr (vars `Set.union` argSet) expr
+       return (tag, args, expr')
 
 -- Just get free variables for annotated AST node
 freeVarsOf :: Annotated Name (Set.Set Name) -> Set.Set Name
 freeVarsOf (vars, _) = vars
+
+-- Just get free variables for alternative
+freeVarsOfAlt :: FVAlt -> Set.Set Name
+freeVarsOfAlt (tag, args, rhs) = freeVarsOf rhs `Set.difference` Set.fromList args
+
+-- Turn list of sets into a single set
+flattenSet :: Ord a => [Set.Set a] -> Set.Set a
+flattenSet = foldl' Set.union Set.empty
 
