73e1ae6fb816f31d58434726dfd2fc8f36c0fe32
Christopher D. Parks <christopher.daniel.parks@gmail.com>
2013-09-07 09:24:48 -0700

    Add tracing to MiniCore/Inference.hs and improve error messages with context

--- src/MiniCore/Inference.hs
+++ src/MiniCore/Inference.hs
@@ -51,9 +51,10 @@ data Mode = Quiet
 
 -- Internal type-inference state
 data TIState = TIState
-    { tiMode :: Mode                 -- Do we print types as we type-check?
+    { tiDecl :: Maybe Declaration    -- Typechecking Context
+    , tiMode :: Mode                 -- Do we print types as we type-check?
     , tiNext :: Int                  -- Generate fresh type variables
-    , tiCons :: Map.Map Name Scheme  -- Constructor for case-expressions
+    , tiCons :: TypeEnv              -- Constructor for case-expressions
     , tiData :: Map.Map Name Type    -- Available types
     }
 
@@ -61,6 +62,60 @@ data TIState = TIState
 --type TI a = ErrorT String (StateT TIState Identity) a
 --type TI a = ErrorT String (StateT TIState IO) a
 type TI a = StateT TIState Stage a
+
+{- Manipulating TI context -} 
+
+-- Set declaration as current typechecking context
+setContext :: Declaration -> TI ()
+setContext decl =
+    do modify $ \s -> s { tiDecl = Just decl }
+
+-- Make context declaration out of name and expression
+setNamedExpr :: Name -> Expr -> TI ()
+setNamedExpr name expr =
+    do let decl = case expr of
+             Lambda args body -> Combinator name args body
+             _                -> Combinator name [] expr
+       setContext decl
+
+-- Throw an error after adding context information
+raise :: String -> TI a
+raise msg =
+    do decl <- gets tiDecl
+       let error = case decl of
+             Just decl -> msg ++ "\nin:\n\t" ++ show (format decl)
+             Nothing   -> msg
+       throwError $ "Type Error:\n\t" ++ error
+    
+-- Print types of specified names in environment if in Trace mode
+tcTrace :: TypeEnv -> [Name] -> TI ()
+tcTrace env names = tcTraceEnv $ fromKeys env names
+
+-- Print all types in environment if in Trace mode
+tcTraceEnv :: TypeEnv -> TI ()
+tcTraceEnv env = 
+    do mode <- gets tiMode
+       case mode of
+           Trace -> mapM_ traceType $ Map.toList env
+           Quiet -> return ()
+  where
+    traceType (name, t) =
+        liftIO $ putStrLn (name ++ " :: " ++  prettyScheme t)
+
+{- Utilities -}
+
+-- Used to quote entities we're printing
+quote :: String -> String
+quote s = "'" ++ s ++ "'"
+
+-- Make a new Map just using the specified keys
+fromKeys m []     = Map.empty
+fromKeys m (x:xs) =
+    case Map.lookup x m of
+        Just v  -> Map.insert x v (fromKeys m xs)
+        Nothing -> fromKeys m xs
+
+{- Dealing with type variable substitution -}
 
 -- Substitute type in for name
 type Subst = Map.Map Name Type
@@ -108,6 +163,8 @@ scomp :: Subst -> Subst -> Subst
 scomp s1 s2 = let s2' = Map.map (apply s1) s2
               in s2' `Map.union` s1
 
+{- Pretty-printing for types -}
+
 -- Infinite list of prettier type-variables for printTying
 prettyVars :: [String]
 prettyVars = alphas ++ alphaNums
@@ -119,21 +176,17 @@ prettyVars = alphas ++ alphaNums
                char <- ['a'..'z']
                return $ char:show num
 
--- Pretty-print a concrete type
-prettyType :: Type -> String
-prettyType t = let env = zip (tvarsOrdered t) prettyVars
-               in prettyType' env t
-
 -- Pretty-print a universally-quantified type
 prettyScheme :: Scheme -> String
 prettyScheme (Scheme vars t) =
     let env = zip (nub (vars ++ tvarsOrdered t)) prettyVars
-    in prettyForall env vars ++ prettyType' env t
+    in prettyForall env vars ++ prettyType env t
 
--- Shared code for printing a type with a mapping from the actual
+-- Print a type using a mapping from the actual
 -- type-variables to pretty type-variables
-prettyType' :: [(Name, Name)] -> Type -> String
-prettyType' env t = loop t where
+prettyType :: [(Name, Name)] -> Type -> String
+prettyType env t = loop t
+  where
     loop (TVar n) =
         case lookup n env of
             Just x  -> x
@@ -147,14 +200,18 @@ prettyType' env t = loop t where
 prettyForall :: [(Name, Name)] -> [Name] -> String
 prettyForall env [] = ""
 prettyForall env xs = "forall " ++ intercalate " " (map get xs) ++ ". "
-  where get n = case lookup n env of
-                  Just x  -> x
-                  Nothing -> "?"
+  where
+    get n =
+        case lookup n env of
+            Just x  -> x
+            Nothing -> "?"
 
 -- Pretty-print all type-schemes in a type-environment
 prettyEnv :: TypeEnv -> String
 prettyEnv env = intercalate "\n" (map pretty (Map.toList env)) ++ "\n"
   where pretty (name, scheme) = name ++ " :: " ++ prettyScheme scheme
+
+{- Unification -}
 
 -- Identity substition has no effect when applied
 idSubst :: Subst
@@ -165,7 +222,7 @@ idSubst = Map.empty
 extend :: Subst -> Name -> Type -> TI Subst
 extend s n t
     | t == TVar n            = return s
-    | n `Set.member` tvars t = throwError "occurs check failed"
+    | n `Set.member` tvars t = raise "Cannot construct infinite type"
     | otherwise              = return $ Map.singleton n t `scomp` s
 
 -- Find a substituion that unifies pair of types.
@@ -180,18 +237,20 @@ unify s (TCon n ts, TVar n') =
     unify s (TVar n', TCon n ts)
 unify s (TCon n ts, TCon n' ts')
     | n == n'   = unifyAll s (ts `zip` ts')
-    | otherwise = throwError $
-                      "Cannot unify " ++ show n ++ " and " ++ show n'
+    | otherwise = raise $ "Cannot unify " ++ quote n ++ " and " ++ quote n'
 
 -- Unify each pair of types propagating the substition
 unifyAll :: Subst -> [(Type, Type)] -> TI Subst
 unifyAll = foldM unify
 
+{- Generating new type variables -}
+
 -- Get a new name
 fresh :: TI Name
-fresh = do n <- gets tiNext
-           modify $ \s -> s { tiNext = n + 1 }
-           return $ "t" ++ show n
+fresh =
+    do n <- gets tiNext
+       modify $ \s -> s { tiNext = n + 1 }
+       return $ "t" ++ show n
 
 -- Given a list of names, return a substitution that maps
 -- the old names to completely new ones
@@ -201,6 +260,68 @@ withNames = foldM newName Map.empty
     newName env name =
         do x <- fresh
            return $ Map.insert name (TVar x) env
+
+{- Type checking for data declarations -}
+
+-- Add a new data type to the tiData mapping
+newDataType :: Name -> Type -> TI ()
+newDataType name t =
+    do types <- gets tiData
+       when (name `Map.member` types) $
+           raise $ "Redeclaration of data type " ++ quote name
+       modify $ \s -> s { tiData = Map.insert name t types }
+
+-- Ensure each data declaration is well-formed:
+-- * Must not be redeclaration
+-- * Each constructor must be well-formed
+-- * Type variables are replaced with fresh type variables
+checkDataTypes :: TypeEnv -> [Declaration] -> TI TypeEnv
+checkDataTypes = foldM checkDataType where
+    checkDataType env decl@(Data name vars constructors) =
+        do setContext decl
+           s <- withNames vars
+           let ty       = apply s $ TCon name $ map TVar vars
+               vars'    = tvars ty
+               mkScheme = Scheme (Set.toList vars') . foldr arrow ty . apply s
+           newDataType name ty
+           checkConstructors mkScheme (Set.fromList vars) env constructors
+
+-- Ensure each constructor for a data type is well-formed
+-- * Must not be a redeclaration
+-- * Type-variables must be in scope
+-- * Composite constructors must use already-defined types
+checkConstructors :: ([Type] -> Scheme) -> Set.Set Name -> TypeEnv -> [Constructor] -> TI TypeEnv
+checkConstructors mkScheme vars = foldM checkConstructor where
+    checkConstructor env (Constructor name args) =
+        do when (not (tvars args `Set.isSubsetOf` vars)) $
+               raise $ "Unbound type variable in constructor " ++ quote name
+           cons <- gets tiCons
+           when (name `Map.member` cons) $
+               raise $ "Redeclaration of constructor " ++ quote name
+           mapM_ validType args
+           let scheme = mkScheme args
+           modify $ \s -> s { tiCons = Map.insert name scheme cons }
+           return $ Map.insert name scheme env
+
+    validType (TVar _) = return ()
+    validType (TCon n ts) =
+        do mapM_ validType ts
+           types <- gets tiData
+           when (n `Map.notMember` types) $
+               raise $ "Use of undeclared type " ++ quote n
+           return ()
+
+{- Type checking for bindings and expressions -}
+
+-- Type check a list of expressions producing a composite substition
+-- and a list of types
+tcAll :: TypeEnv -> [(Name, Expr)] -> TI (Subst, [Type])
+tcAll env []                = return (idSubst, [])
+tcAll env ((name, expr):es) =
+    do setNamedExpr name expr
+       (s, t)   <- tcExpr env expr
+       (s', ts) <- tcAll (apply s env) es
+       return (s' `scomp` s, apply s' t : ts)
 
 -- Type-check an expression in the current type-environment producing
 -- a substitution and a concrete type
@@ -212,16 +333,19 @@ tcExpr env (Num _) = return (idSubst, intTy)
 -- Variable
 tcExpr env (Var x) =
     do scheme <- case Map.lookup x env of
-           Nothing     -> throwError $ "Unbound variable: " ++ x
+           Nothing     -> raise $ "Unbound variable " ++ quote x
            Just scheme -> return scheme
        t <- newInstance scheme
        return (idSubst, t)
 
 -- Application 
 tcExpr env (App e1 e2) =
-    do (s, [t1, t2]) <- tcAll env [e1, e2]
+    do (s1, t1) <- tcExpr env e1
+       (s2, t2) <- tcExpr env e2
+       let t1' = apply s2 t1
+           s   = s2 `scomp` s1
        n  <- fresh
-       s' <- unify s (t1, t2 `arrow` TVar n)
+       s' <- unify s (t1', t2 `arrow` TVar n)
        return (s', apply s' (TVar n))
 
 -- Binary application
@@ -246,14 +370,14 @@ tcExpr env (Let False bindings expr) =
     do let (names, exprs) = unzip bindings
 
        -- Infer monomorphic types for definitions
-       (s, ts) <- tcAll env exprs
+       (s, ts) <- tcAll env bindings
 
        -- Generalize types and add to type-environment
        env'' <- addDecls (apply s env) names ts
 
        -- Infer type for body
        (s', t) <- tcExpr env'' expr
-       --tcTrace (apply s' env'') names
+       tcTrace (apply s' env'') names
        return (s' `scomp` s, t)
 
 -- Recursive Let
@@ -264,7 +388,7 @@ tcExpr env (Let True bindings expr) =
        schemes <- toSchemes names
 
        -- Infer monomorphic types for definitions
-       (s, ts) <- tcAll (schemes `Map.union` env) exprs
+       (s, ts) <- tcAll (schemes `Map.union` env) bindings
 
        -- Apply substition to type-variables and unify
        -- with inferred types
@@ -277,22 +401,8 @@ tcExpr env (Let True bindings expr) =
        -- Add types to environment and infer type for body
        env'' <- addDecls (apply s' env') names ts''
        (s'', t) <- tcExpr env'' expr
-       --tcTrace (apply s'' env'') names
+       tcTrace (apply s'' env'') names
        return (s'' `scomp` s', t)
-     
-{-
-tcTrace :: TypeEnv -> [Name] -> TI ()
-tcTrace env names =
-    do mode <- gets tiMode
-       case mode of
-           Trace -> mapM_ traceType names
-           Quiet -> return ()
-  where
-    traceType name =
-        do case Map.lookup name env of
-             Just t  -> liftIO $ putStrLn (name ++ " :: " ++  prettyScheme t)
-             Nothing -> return ()
--}
 
 -- Type-check each alternative in a case-expression
 tcAlts :: TypeEnv -> Type -> [Alt] -> TI (Subst, Type)
@@ -305,29 +415,31 @@ tcAlts env rtype alts =
            s'' <- unify s' (t, t')
            return (s'' `scomp` s, t')
 
--- Break a constructor into its components and return type
-components :: Type -> ([Type], Type)
-components = loop [] where
-    loop cs (TCon "(->)" [a, b]) = loop (a:cs) b
-    loop cs last                  = (reverse cs, last)
-
 -- Type-check a single alternative
 tcAlt :: TypeEnv -> Type -> Alt -> TI (Subst, Type)
 tcAlt env t (PCon name, names, body) =
     do cons <- gets tiCons
        scheme <- case Map.lookup name cons of
            Just x  -> return x
-           Nothing -> throwError $ "Undeclared constructor " ++ name
+           Nothing -> raise $ "Undeclared constructor " ++ quote name
        t' <- newInstance scheme
        let (types, rtype) = components t'
        s <- unify idSubst (t, rtype)
        when (length types /= length names) $
-           throwError $ "Wrong number of components for " ++ name ++ " in case-expression"
+           raise $ "Wrong number of components for " ++ quote name
        let schemes = map (Scheme []) types
            bindings = zip names schemes
            env'     = Map.fromList bindings `Map.union` env
        (s', t') <- tcExpr env' body
        return (s', t')
+  where
+    -- Break a constructor into its components and return type
+    components :: Type -> ([Type], Type)
+    components = loop [] where
+        loop cs (TCon "(->)" [a, b]) = loop (a:cs) b
+        loop cs last                  = (reverse cs, last)
+
+{- Dealing with generalization and instantiation -}
 
 -- Generate a mapping from names to new type schemes
 toSchemes :: [Name] -> TI TypeEnv
@@ -373,21 +485,15 @@ addDecls env ns ts =
     do schemes <- mapM (generalize env) ts
        return $ Map.fromList (zip ns schemes) `Map.union` env
 
--- Type check a list of expressions producing a composite substition
--- and a list of types
-tcAll :: TypeEnv -> [Expr] -> TI (Subst, [Type])
-tcAll env []     = return (idSubst, [])
-tcAll env (e:es) =
-    do (s, t)   <- tcExpr env e
-       (s', ts) <- tcAll (apply s env) es
-       return (s' `scomp` s, apply s' t : ts)
+{- Build entry point to type inference engine -}
 
 -- Run type-inference and return either an error or some value
 runTI :: Mode -> TI a -> Stage a
 runTI mode ti = evalStateT ti initState
   where
     initState = TIState
-        { tiMode = mode
+        { tiDecl = Nothing
+        , tiMode = mode
         , tiNext = 0
         , tiCons = Map.fromList
                     [ ("True",  Scheme [] boolTy)
@@ -399,53 +505,6 @@ runTI mode ti = evalStateT ti initState
                     ]
         }
 
--- Add a new data type to the tiData mapping
-newDataType :: Name -> Type -> TI ()
-newDataType name t =
-    do types <- gets tiData
-       when (name `Map.member` types) $
-           throwError $ "Redeclaration of data type " ++ name
-       modify $ \s -> s { tiData = Map.insert name t types }
-
--- Ensure each data declaration is well-formed:
--- * Must not be redeclaration
--- * Each constructor must be well-formed
--- * Type variables are replaced with fresh type variables
-checkDataTypes :: TypeEnv -> [Declaration] -> TI TypeEnv
-checkDataTypes = foldM checkDataType where
-    checkDataType env (Data name vars constructors) =
-        do s <- withNames vars
-           let ty       = apply s $ TCon name $ map TVar vars
-               vars'    = tvars ty
-               mkScheme = Scheme (Set.toList vars') . foldr arrow ty . apply s
-           newDataType name ty
-           checkConstructors mkScheme (Set.fromList vars) env constructors
-
--- Ensure each constructor for a data type is well-formed
--- * Must not be a redeclaration
--- * Type-variables must be in scope
--- * Composite constructors must use already-defined types
-checkConstructors :: ([Type] -> Scheme) -> Set.Set Name -> TypeEnv -> [Constructor] -> TI TypeEnv
-checkConstructors mkScheme vars = foldM checkConstructor where
-    checkConstructor env (Constructor name args) =
-        do when (not (tvars args `Set.isSubsetOf` vars)) $
-               throwError $ "Unbound type variable in constructor " ++ name
-           cons <- gets tiCons
-           when (name `Map.member` cons) $
-               throwError $ "Redeclaration of constructor " ++ name
-           mapM_ validType args
-           let scheme = mkScheme args
-           modify $ \s -> s { tiCons = Map.insert name scheme cons }
-           return $ Map.insert name scheme env
-
-    validType (TVar _) = return ()
-    validType (TCon n ts) =
-        do mapM_ validType ts
-           types <- gets tiData
-           when (n `Map.notMember` types) $
-               throwError $ "Use of undeclared type " ++ n
-           return ()
-
 -- Convert a list of declarations into a single letrec
 -- Each non-nullary binding is turned into a lambda
 -- This is also where we check for the existence of main
@@ -455,11 +514,11 @@ convertToExpr combinators =
        case (bindings, main) of
            ([],       Just main) -> return main
            (bindings, Just main) -> return $ Let True bindings main
-           (_,        Nothing)   -> throwError "Must specify nullary function main"
+           (_,        Nothing)   -> raise "Must specify nullary function main"
   where
     -- Redeclaring main is an error
     convert (xs, Just main) (Combinator "main" []   expr)
-        = throwError "Redeclaration of main"
+        = raise "Redeclaration of main"
 
     -- Initial declaration of main
     convert (xs, Nothing) (Combinator "main" [] expr)
@@ -467,7 +526,7 @@ convertToExpr combinators =
 
     -- Declaring main with arguments is an error
     convert (xs, Nothing) (Combinator "main" args expr)
-        = throwError "main must be a nullary declaration"
+        = raise "main must be a nullary declaration"
 
     -- A binding with no arguments just becomes a regular let-binding
     convert (xs, main) (Combinator name [] expr)
@@ -485,5 +544,7 @@ inferTypes program =
        env' <- checkDataTypes env decls
        expr <- lift . simplifyExpr =<< convertToExpr combinators
        (s, t) <- tcExpr env' expr
+       cons <- gets tiCons
+       tcTraceEnv cons
        return $ decls ++ combinators
 
